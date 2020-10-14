396608c8d05cec28ad75d98b708186bfb7f31ea3
Hammad-23 <hsyed2633@gmail.com>
2020-10-14 02:51:17 -0700

    log in UI

--- App.js
+++ App.js
@@ -5,34 +5,39 @@ import { StyleSheet, Text, View, TextInput, Button,ImageBackground } from 'react
 
 export default function App() {
 
-  const image = { uri: "https://reactjs.org/logo-og.png" };
+  const image = { uri: "https://st.depositphotos.com/1163607/1682/v/450/depositphotos_16827173-stock-illustration-blood-donation.jpg" };
   return (
+    <ImageBackground source={image} style={styles.image}>
     <View style={styles.container}>
-      <ImageBackground source={image} style={styles.image}>
+     
       <View style={{marginBottom: 200,backgroundColor: 'lightblue'}}>
         <Text style={{color: 'red', fontSize: 30, fontFamily: 'sans-serif', fontWeight:"bold"}}>Blood Donation App</Text>
       </View>
      <View>
        <View>
-         <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1, maxWidth: 250, minWidth: 250}} placeholder='Write email here'/>
+         <TextInput style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} placeholder='Write email here'/>
        </View>
        <View style={{marginTop: 30}}>
-         <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1, maxWidth: 250, minWidth: 250}} secureTextEntry={true}  placeholder='Write password here'/>
+         <TextInput style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} secureTextEntry={true}  placeholder='Write password here'/>
        </View>
        <View style={{marginTop: 30, width: 80, marginLeft: 80}}>
-         <Button  title='Log In'/>
+         <Button  title='Log In' color='green' />
+       </View>
+       <View style={{marginTop: 20}}>
+         <Button title='LogIn with Facebook'/>
        </View>
      </View>
       <StatusBar style="auto" />
-      </ImageBackground>
+     
     </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 0.8,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     
