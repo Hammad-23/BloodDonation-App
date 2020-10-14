2d099a095cf4ecef8e63611eab02fe1364a9298a
Hammad-23 <hsyed2633@gmail.com>
2020-10-14 03:18:38 -0700

    log in details

--- App.js
+++ App.js
@@ -1,27 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button,ImageBackground } from 'react-native';
 // import Blood from './assets/image/blood.jpg'
 
 export default function App() {
+
+  const [username,setUsername] = useState()
+
+  
+
+  const val = function(enteredText){
+    const name =(enteredText.nativeEvent.text)
+    
+    setUsername({email:name})
+
+    // console.log(username)
+    
+  }
+
+  const pass= (enteredText)=>{
+
+    const code =(enteredText.nativeEvent.text)
+    setUsername({password:code})
+    console.log(username)
+   
+  }
+    
+  // const logIn = ()=>{
+  //    localStorage.setItem(username)
+  // }
 
   const image = { uri: "https://st.depositphotos.com/1163607/1682/v/450/depositphotos_16827173-stock-illustration-blood-donation.jpg" };
   return (
     <ImageBackground source={image} style={styles.image}>
     <View style={styles.container}>
      
-      <View style={{marginBottom: 200,backgroundColor: 'lightblue'}}>
+      <View style={{marginBottom: 200,backgroundColor: 'lightblue',borderRadius:10}}>
         <Text style={{color: 'red', fontSize: 30, fontFamily: 'sans-serif', fontWeight:"bold"}}>Blood Donation App</Text>
       </View>
      <View>
        <View>
-         <TextInput style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} placeholder='Write email here'/>
+         <TextInput onChange={val}
+
+      
+         
+         style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} placeholder='Write email here'/>
        </View>
        <View style={{marginTop: 30}}>
-         <TextInput style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} secureTextEntry={true}  placeholder='Write password here'/>
+         <TextInput onChange={pass} style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} secureTextEntry={true}  placeholder='Write password here'/>
        </View>
        <View style={{marginTop: 30, width: 80, marginLeft: 80}}>
-         <Button  title='Log In' color='green' />
+         <Button   title='Log In' color='green' />
        </View>
        <View style={{marginTop: 20}}>
          <Button title='LogIn with Facebook'/>
