6e3ccc965c020db13be0df140c282cb2fa8492eb
Hammad-23 <hsyed2633@gmail.com>
2020-10-15 07:42:16 -0700

    save in storage

--- screen/signup/index.js
+++ screen/signup/index.js
@@ -1,23 +1,48 @@
 import React, { useState } from 'react';
+// import SyncStorage from 'sync-storage';
+import { AsyncStorage } from 'react-native';
 import { StyleSheet, Text, View, TextInput, Button,ImageBackground } from 'react-native';
 
 
 function SignUp(props){
+
+    const [info,setInfo] =useState()
+
+    const fullname=(enteredText)=>{
+        const name = (enteredText.nativeEvent.text)
+        setInfo({fullName: name})
+    }
+
+    const userEmail=(enteredText)=>{
+        const email = (enteredText.nativeEvent.text)
+        setInfo({user: email})
+    }
+
+    const userPass=(enteredText)=>{
+        const password = (enteredText.nativeEvent.text)
+        setInfo({pass: password})
+    }
+
+    const signup =()=>{
+        AsyncStorage.setItem('userInfo',info);
+        alert('wait ho raha hai')
+    }
+
     return(
         <>
         <View style={{alignItems:"center",marginTop:250}}>
 
             <View>
-                <TextInput style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} placeholder='Enter FullName '/>
+                <TextInput onChange={fullname} style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} placeholder='Enter FullName '/>
             </View>
             <View style={{marginTop: 30}}>
-                <TextInput style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} placeholder='Enter Email'/>
+                <TextInput onChange={userEmail} style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} placeholder='Enter Email'/>
             </View>
             <View style={{marginTop: 30}}>
-                <TextInput style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} secureTextEntry={true} placeholder='Enter Password'/>
+                <TextInput onChange={userPass} style={{height: 40, borderColor: 'green', borderWidth: 1, maxWidth: 250, minWidth: 250,borderRadius:10}} secureTextEntry={true} placeholder='Enter Password'/>
             </View>
             <View style={{marginTop: 30}}>
-                <Button color='green' title='Sign Up'/>
+                <Button onPress={signup} color='green' title='Sign Up'/>
             </View>
             <View style={{marginTop:20}}>
             <Button onPress={()=>{props.setLog(true)
