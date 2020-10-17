b941a67d558cf140b39ddad8cdb7546d9d837cff
Hammad-23 <hsyed2633@gmail.com>
2020-10-17 08:03:05 -0700

    more

--- screen/home/index.js
+++ screen/home/index.js
@@ -1,22 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button,ImageBackground,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button,ImageBackground,TouchableOpacity,ScrollView } from 'react-native';
 
 
 function Home(){
     return(
         <>
-        <View style={{backgroundColor: 'green',height:50,display: "flex",justifyContent:"space-between"}}>
+        <ScrollView>
+        
+        <View style={{backgroundColor: 'green',height:50,display: "flex",justifyContent:"space-between",marginTop:50}}>
             <Text style={{color:'black'}}>Home Page</Text>
             <Text style={{color:'black'}}>LogOut</Text>
         </View>
 
-        <View>
+        <View style={{marginTop: 100}}>
+            <Button color='red' title='View Blood Donors'/>
+            <Button color='red' title='Ask about donate'/>
+        </View>
+        <View style={{marginTop: 75}}>
+            <Button color='red' title='View Blood Donors'/>
+            <Button color='red' title='Ask about donate'/>
+        </View>
+        <View style={{marginTop: 75}}>
+            <Button color='red' title='View Blood Donors'/>
+            <Button color='red' title='Ask about donate'/>
+        </View>
+        <View style={{marginTop: 75}}>
             <Button color='red' title='View Blood Donors'/>
             <Button color='red' title='Ask about donate'/>
         </View>
 
-
+        </ScrollView>
         </>
     )
 }
